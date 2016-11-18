8e0be0c9072fbad562fcabc4507dc479ca807b2d
DIVYA SAMTANI <Divyasamtani@gmail.com>
2016-11-18 17:31:28 +0800

    heroku link

--- www/js/angular/services/constant.js
+++ www/js/angular/services/constant.js
@@ -1,3 +1,4 @@
 app.constant('urlConstant', {
-  apiUrl: navigator.platform == 'MacIntel' ? 'http://localhost:3000' : 'http://zealist.herokuapp.com',
+  apiUrl: navigator.platform == 'MacIntel' ? 'http://localhost:3000' : 'http://wdi-12-project-4-divya.herokuapp.com',
+  // apiUrl: 'https://wdi-12-project-4-divya.herokuapp.com',
 });
