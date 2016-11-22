84db1f7be1902eb21f2b0dab70c3928ea1e1d8ca
DIVYA SAMTANI <Divyasamtani@gmail.com>
2016-11-22 17:19:45 +0800

    ionic resources

--- www/js/angular/services/constant.js
+++ www/js/angular/services/constant.js
@@ -1,5 +1,5 @@
 app.constant('urlConstant', {
-  apiUrl: navigator.platform == 'MacIntel' ? 'http://localhost:3000' : 'http://www.zealist.io',
+  // apiUrl: navigator.platform == 'MacIntel' ? 'http://localhost:3000' : 'http://www.zealist.io',
   // apiUrl: 'http://localhost:3000'
-  // apiUrl: 'http://www.zealist.io',
+  apiUrl: 'http://www.zealist.io',
 });
